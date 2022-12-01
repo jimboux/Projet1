77d818181dea7f110e33d34414063a4d696ba7a4
jimboux <100678513+jimboux@users.noreply.github.com>
2022-12-01 14:38:01 +0100

    html reflexo.html

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let mybutton = document.getElementById("myBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
