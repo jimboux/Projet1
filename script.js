591592fb63325542882330b6489c449cf43173cc
jimboux <100678513+jimboux@users.noreply.github.com>
2022-12-07 13:14:32 +0100

    menu burger

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 // Get the button:
 let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 600px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
-
 function scrollFunction() {
   if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
     mybutton.style.display = "block";
@@ -11,9 +9,17 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// MENU BURGER 
+const icon = document.querySelector(".fa-bars");
+const list = document.querySelector(".nav-list-mobile");
+
+icon.addEventListener("click", function(){
+  console.log("pwet");
+  list.classList.toggle("list-visible");
+});
